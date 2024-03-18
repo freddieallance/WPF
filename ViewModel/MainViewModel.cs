330e30d504fec3a79900d605debde96b5bcb3148
freddieallance <122672447+freddieallance@users.noreply.github.com>
2024-03-18 09:56:26 +0800

    Test

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp1.Model;
+using WpfApp1.Repositories;
+
+namespace WpfApp1.ViewModel
+{
+    public class MainViewModel: ViewModelBase
+    {
+        //Fields
+        private UserAccountModel _currentUserAccount;
+        private IUserRepository userRepository;
+
+        public UserAccountModel CurrentUserAccount 
+        { get
+            {  
+                return _currentUserAccount; 
+            
+            }
+            
+            set 
+            { 
+                _currentUserAccount = value;
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            
+            }
+           }
+        public MainViewModel()
+        {
+            userRepository = new UserRepository();
+            LoadCurrentUserData();
+        }
+
+        private void LoadCurrentUserData()
+        {
+            var user = userRepository.GetUserModel(Thread.CurrentPrincipal.Identity.Name);
+            if(user!=null)
+            {
+                CurrentUserAccount = new UserAccountModel()
+                { 
+                    Username=user.Username,
+                    DisplayName=$"Welcome {user.Name} {user.LastName} ;)",
+                    ProfilePicture=null
+                };
+            }
+            else 
+            {
+                MessageBox.Show("Invalid user, not logged in");
+                Application.Current.Shutdown(); 
+            }
+        }
+    }
+}
