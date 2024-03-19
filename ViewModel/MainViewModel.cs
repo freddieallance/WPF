943346dc4ee7bf66eec52616f22ce8e9bdf3c404
freddieallance <122672447+freddieallance@users.noreply.github.com>
2024-03-19 08:35:44 +0800

    update

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using WpfApp1.Model;
 using WpfApp1.Repositories;
 
@@ -13,8 +15,13 @@ namespace WpfApp1.ViewModel
     {
         //Fields
         private UserAccountModel _currentUserAccount;
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
         private IUserRepository userRepository;
 
+        //Properties
         public UserAccountModel CurrentUserAccount 
         { get
             {  
@@ -29,10 +36,79 @@ namespace WpfApp1.ViewModel
             
             }
            }
+
+        public ViewModelBase CurrentChildView 
+        { 
+            get 
+            {
+                return _currentChildView;
+            }
+            
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            }
+            
+        }
+        public string Caption  
+        { 
+            get
+            {  
+                return _caption; 
+            }
+            
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+                
+        }
+        public IconChar Icon 
+        {
+            get
+            { 
+                return _icon; 
+            }
+            set
+            { 
+                _icon = value; 
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+        //--> Commands
+        public ICommand ShowHomeViewCommand { get; }
+        public ICommand ShowCustomerViewCommand { get; }
+
         public MainViewModel()
         {
             userRepository = new UserRepository();
+            CurrentUserAccount = new UserAccountModel();
+
+            //Initialize commands
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
+
+            //Default View
+            ExecuteShowHomeViewCommand(null);
+
             LoadCurrentUserData();
+        }
+
+        private void ExecuteShowCustomerViewCommand(object obj)
+        {
+            CurrentChildView = new CustomerViewModel();
+            Caption = "Customers";
+            Icon = IconChar.UserGroup;
+        }
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            Caption = "Dashboard";
+            Icon = IconChar.Home;
         }
 
         private void LoadCurrentUserData()
